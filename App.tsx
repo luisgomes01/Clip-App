50950b0707efc20d0b05a4d74f2bd82d06332c1c
Luís Gomes <luis252001@hotmail.com>
2022-11-24 00:37:41 -0300

    feat/base-theme add base theme to the application

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ThemeContext } from "./src/contexts/ThemeContext";
+import Application from "./src/index";
 
 const App = () => (
-  <View style={styles.container}>
-    <Text>Open up App.tsx to start working on your app!</Text>
-    <StatusBar style="auto" />
-  </View>
+  <ThemeContext>
+    <Application />
+  </ThemeContext>
 );
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default App;
---
+++ src/styles/theme.ts
@@ -0,0 +1,21 @@
+export const lightTheme = {
+  name: "light",
+  primary: "#FFD000",
+  background: "#FEFEFE",
+  titles: "#0C0C0C",
+  text: "#1B1B1B",
+  accent: "#FFE572",
+  listItem: "#E8E8E8",
+  paid: "#00D265",
+};
+
+export const darkTheme = {
+  name: "dark",
+  primary: "#FFD000",
+  background: "#FFFFFF",
+  titles: "#0C0C0C",
+  text: "#1B1B1B",
+  accent: "#FFE572",
+  listItem: "#E8E8E8",
+  paid: "#00D265",
+};
