db592ea422cf5b9d453dbb4161a2d0a069c9cba5
Luís Gomes <luis252001@hotmail.com>
2022-11-25 12:07:24 -0300

    fix: fix ButtonComponent props

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,12 +1,14 @@
 import { ReactNode } from "react";
+import { TouchableOpacityProps } from "react-native";
 import { Button, Text } from "./Button.styles";
 
-interface Props {
+interface Props extends TouchableOpacityProps {
   children: ReactNode;
   type: "primary" | "secondary";
 }
-const ButtonComponent = ({ children, type }: Props) => (
-  <Button type={type}>
+
+const ButtonComponent = ({ children, type, ...otherProps }: Props) => (
+  <Button type={type} {...otherProps}>
     <Text type={type}>{children}</Text>
   </Button>
 );
