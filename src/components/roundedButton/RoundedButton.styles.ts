9d53084d4ebf093bad88d95f8eafcd8a676598c3
Luís Gomes <luis252001@hotmail.com>
2022-11-26 00:18:09 -0300

    feat: add styling to rounded button component

---
+++ src/components/roundedButton/RoundedButton.styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components/native";
+
+interface Props {
+  type: "primary" | "secondary";
+}
+
+export const RoundButton = styled.TouchableOpacity<Props>`
+  background-color: ${(props) =>
+    props.type === "primary" ? props.theme.primary : props.theme.background};
+  align-items: center;
+  width: 56px;
+  height: 56px;
+  border-radius: 1000;
+`;
+
+export const PlusIcon = styled.Text`
+  font-size: 40px;
+  color: ${(props) => props.theme.background};
+`;
