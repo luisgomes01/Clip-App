bce622ddc21a46fce5ed55aa26d82faa9468915c
Filipe Melo <filipe.melo@liferay.com>
2022-11-25 21:36:48 -0300

    feat(Navigation): create navigation components

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
 import { View } from "react-native";
 import { useTheme } from "styled-components";
-import Button from "./components/button/Button";
-import Checkbox from "./components/checkbox/Checkbox";
-import DatePicker from "./components/datepicker/DatePicker";
-import Input from "./components/input/Input";
-import PickerSelect from "./components/picker/Picker";
-import RoundedButton from "./components/roundedButton/RoundedButton";
-import Title from "./components/title/Title";
+import { NavigationContainer } from "@react-navigation/native";
+
+import Router from "./router";
 
 export const Application = () => {
   const theme = useTheme();
-  const [date, setDate] = useState(new Date());
 
   return (
     <View
@@ -22,45 +16,9 @@ export const Application = () => {
       }}
     >
       <StatusBar backgroundColor={theme.primary} style={theme.name} />
-      <View
-        style={{
-          marginTop: 50,
-          paddingLeft: 16,
-          paddingRight: 16,
-        }}
-      >
-        <Title>Criar Grupo</Title>
-        <Input label="Descrição" placeholder="Descrição do Grupo" />
-        <Checkbox
-          checked={false}
-          label="Pago"
-          containerStyle={{ marginTop: 15 }}
-        />
-        <Button type="primary">Salvar</Button>
-        <Button type="secondary">Voltar</Button>
-        <PickerSelect
-          items={[
-            {
-              label: "Java",
-              value: "java",
-            },
-            {
-              label: "Javascript",
-              value: "javascript",
-            },
-          ]}
-        />
-        <RoundedButton type="primary" />
-        <DatePicker
-          label="Data da conta"
-          value={date}
-          onChange={() => console.log("Changing datepicker value")}
-          type="set"
-          nativeEvent={{
-            timestamp: 0,
-          }}
-        />
-      </View>
+      <NavigationContainer>
+        <Router />
+      </NavigationContainer>
     </View>
   );
 };
---
+++ src/router/index.tsx
@@ -0,0 +1,34 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useTheme } from "styled-components";
+import CreateBill from "../pages/createBill";
+import CreateGroup from "../pages/createGroup";
+import GroupDetails from "../pages/groupDetails";
+import Search from "../pages/search";
+
+const Stack = createNativeStackNavigator();
+
+const Router = () => {
+  const theme = useTheme();
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: theme.primary,
+        },
+        headerTitle: "Clip",
+        headerTitleStyle: {
+          color: theme.background,
+          fontFamily: "montserrat-bold",
+          fontSize: 32,
+        },
+      }}
+    >
+      <Stack.Screen name="Search" component={Search} />
+      <Stack.Screen name="CreateGroup" component={CreateGroup} />
+      <Stack.Screen name="GroupDetails" component={GroupDetails} />
+      <Stack.Screen name="CreateBill" component={CreateBill} />
+    </Stack.Navigator>
+  );
+};
+
+export default Router;
