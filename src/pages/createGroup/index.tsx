6ad2bb34db11d210edbf4bb173655c43141ff470
Filipe Melo <filipe.melo@liferay.com>
2022-11-26 10:55:38 -0300

    feat(Pages): add create group page

--- src/pages/createGroup/index.tsx
+++ src/pages/createGroup/index.tsx
@@ -1,7 +1,49 @@
-import { Text } from "react-native";
+import Button from "../../components/button/Button";
+import Input from "../../components/input/Input";
+import PickerSelect from "../../components/picker/Picker";
+import Title from "../../components/title/Title";
 
-// import { Container } from './styles';
+import { Container } from "./styles";
 
-const CreateGroup = () => <Text>Create Group</Text>;
+const CreateGroup = () => (
+  <Container>
+    <Title>Criar Grupo</Title>
+    <Input
+      label="Descrição"
+      containerStyle={{ marginTop: 12 }}
+      placeholder="Descrição do Grupo"
+    />
+    <PickerSelect
+      containerStyle={{ marginTop: 12 }}
+      label="Tipo"
+      items={[
+        {
+          label: "Energia",
+          value: "electricity",
+        },
+        {
+          label: "Água",
+          value: "water",
+        },
+        {
+          label: "Internet",
+          value: "internet",
+        },
+        {
+          label: "Telefone",
+          value: "phone",
+        },
+        {
+          label: "Outros Pagamentos",
+          value: "others",
+        },
+      ]}
+    />
+    <Button type="primary" style={{ marginTop: "auto" }}>
+      Salvar
+    </Button>
+    <Button type="secondary">Voltar</Button>
+  </Container>
+);
 
 export default CreateGroup;
