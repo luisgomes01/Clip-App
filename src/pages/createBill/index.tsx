a3295883237fbd20fc87c2bbd323ac4d74752c77
Filipe Melo <filipe.melo@liferay.com>
2022-11-27 21:17:22 -0300

    feat(Pages): Add Create Bill page

--- src/pages/createBill/index.tsx
+++ src/pages/createBill/index.tsx
@@ -1,7 +1,30 @@
-import { Text } from "react-native";
+import Checkbox from "../../components/checkbox/Checkbox";
+import Container from "../../components/container/Container";
+import DatePicker from "../../components/datepicker/DatePicker";
+import Input from "../../components/input/Input";
+import Title from "../../components/title/Title";
+import Button from "../../components/button/Button";
 
-// import { Container } from './styles';
-
-const CreateBill = () => <Text>Create Bill</Text>;
+const CreateBill = () => (
+  <Container>
+    <Title>Adicionar Conta</Title>
+    <Input
+      label="Descrição"
+      containerStyle={{ marginTop: 12 }}
+      placeholder="Descrição da Conta"
+    />
+    <DatePicker
+      containerStyle={{ marginTop: 12 }}
+      label="Data da Conta"
+      value={new Date()}
+      onChange={() => {}}
+    />
+    <Checkbox label="Pago" checked={false} containerStyle={{ marginTop: 12 }} />
+    <Button type="primary" style={{ marginTop: "auto" }}>
+      Salvar
+    </Button>
+    <Button type="secondary">Voltar</Button>
+  </Container>
+);
 
 export default CreateBill;
