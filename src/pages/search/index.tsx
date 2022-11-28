37c8a3ba0831d3ffb58fa5a4fc302aa28a3d91d7
Luís Gomes <luis252001@hotmail.com>
2022-11-28 16:04:28 -0300

    feat: show list of groups on homepage

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,9 +1,16 @@
 import { useTheme } from "styled-components";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { FlatList } from "react-native";
 import Input from "../../components/input/Input";
-import { SearchContainer, Icon } from "./styles";
+import { SearchContainer, Icon, GroupView, GroupText } from "./styles";
 import Container from "../../components/container/Container";
 import RoundedButton from "../../components/roundedButton/RoundedButton";
+import { useClip } from "../../contexts/ClipContext";
+import LampIcon from "../../components/icons/LampIcon";
+import WaterDropIcon from "../../components/icons/WaterDropIcon";
+import InternetIcon from "../../components/icons/InternetIcon";
+import PhoneIcon from "../../components/icons/PhoneIcon";
+import BarCodeIcon from "../../components/icons/BarCodeIcon";
 
 type RootStackParamList = {
   CreateGroup: {};
@@ -14,15 +21,55 @@ type Props = NativeStackScreenProps<RootStackParamList>;
 const Search = ({ navigation }: Props) => {
   const theme = useTheme();
 
+  const { groups } = useClip();
+
+  const ICONS_MAPPING = {
+    electricity: (
+      <LampIcon
+        color={theme.name === "light" ? theme.placeholder : theme.labelColor}
+      />
+    ),
+    water: (
+      <WaterDropIcon
+        color={theme.name === "light" ? theme.placeholder : theme.labelColor}
+      />
+    ),
+    internet: (
+      <InternetIcon
+        color={theme.name === "light" ? theme.placeholder : theme.labelColor}
+      />
+    ),
+    phone: (
+      <PhoneIcon
+        color={theme.name === "light" ? theme.placeholder : theme.labelColor}
+      />
+    ),
+    others: (
+      <BarCodeIcon
+        color={theme.name === "light" ? theme.placeholder : theme.labelColor}
+      />
+    ),
+  };
+
   const handleAddButton = () => {
     navigation.navigate("CreateGroup");
   };
+
   return (
     <Container>
       <SearchContainer>
         <Input placeholder="Pesquisar" />
-        <Icon color={theme.labelColor} />
+        <Icon color={theme.placeholder} />
       </SearchContainer>
+      <FlatList
+        data={groups}
+        renderItem={({ item }) => (
+          <GroupView>
+            <GroupText>{item.description}</GroupText>
+            {ICONS_MAPPING[item.type]}
+          </GroupView>
+        )}
+      />
       <RoundedButton
         type="primary"
         style={{
--- src/pages/search/styles.ts
+++ src/pages/search/styles.ts
@@ -1,8 +1,9 @@
 import styled from "styled-components/native";
-import SearchIcon from "./SearchIcon";
+import SearchIcon from "../../components/icons/SearchIcon";
 
 export const SearchContainer = styled.View`
   position: relative;
+  margin-bottom: 17px;
 `;
 
 export const Icon = styled(SearchIcon)`
@@ -10,4 +11,33 @@ export const Icon = styled(SearchIcon)`
   right: 16px;
   align-self: center;
   top: 18px;
+  color: ${(props) => props.theme.placeholder};
+`;
+
+export const GroupView = styled.View`
+  background-color: ${(props) => props.theme.listItemBackgroundColor};
+  padding-vertical: 12px;
+  padding-horizontal: 16px;
+  font-size: 16px;
+  line-height: 19.5px;
+  border-radius: 8px;
+  border-width: 1px;
+  margin-bottom: 12px;
+  border-color: ${(props) =>
+    props.theme.name === "light"
+      ? props.theme.placeholder
+      : props.theme.listItemBackgroundColor};
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const GroupText = styled.Text`
+  font-size: 20px;
+  line-height: 24.38px;
+  color: ${(props) =>
+    props.theme.name === "light"
+      ? props.theme.placeholder
+      : props.theme.borderColor};
 `;
